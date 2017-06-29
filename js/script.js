2de5a0a4456668dc009614dac9e08413ec5be715
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-06-29 14:16:51 +0200

    update to kaprekar's iteration: passing single array as an argument

--- js/script.js
+++ js/script.js
@@ -1,12 +1,49 @@
 $(function(){
 
-    var arr1 = [5, 2, 0, 0];
-    var arr2 = createMirrorArray(arr1);
+    var givenArray = [5, 2, 0, 0];
+    kaprekarGo(givenArray);
 
-    var num1 = createNumberFromArray(arr1);
-    var num2 = createNumberFromArray(arr2);
 
-    // functions on arrays
+    /*
+    *** KAPREKAR ITERATION ***
+      Step 1. Take any four-digit number, using at least two different digits. (Leading zeros are allowed.)
+      Step 2. Arrange the digits in descending and then in ascending order to get two four-digit numbers, adding leading zeros if necessary.
+      Step 3. Subtract the smaller number from the bigger number.
+      Step 4. Go back to step 2. 
+    */
+
+    function kaprekarGo(array) {
+        var result = 0;
+        var array1 = array; // will be needed for step 2
+        var array2 = createMirrorArray(array1); // will be needed for step 2
+        var num1 = createNumberFromArray(array1);
+        var num2 = createNumberFromArray(array2);
+        result = num1 - num2; // step 3
+        /* 
+        condition: Kaprekar's constant is 6174 
+        */
+        if (result !== 6174) {
+            // step 2
+            result = String(result);
+            var newArray = [];
+            for (var i = 0; i < result.length; i++) {
+                newArray.push(result[i]);
+            }
+            newArray.sort(function(a, b){return b-a});
+            // step 4
+            kaprekarGo(newArray);
+        } else {
+            console.log(result);
+        }
+    }
+
+    /*
+    ** functions on arrays **
+    */
+
+    /*
+    creating mirror array - needed for subtraction
+    */
     function createMirrorArray(inputArray){
         var outputArray = [];
         for (var i = 0; i < inputArray.length; i++) {
@@ -15,6 +52,10 @@ $(function(){
         outputArray.reverse();
         return outputArray;
     }
+
+    /*
+    converting arrays into integers
+    */
 
     function createNumberFromArray(inputArray){
         var numString = "";
@@ -26,27 +67,7 @@ $(function(){
         return result;
     }
 
-    function createArraysFromSubtraction(highNum, lowNum) {
-        var result = 0;
-        var array1 = [];
-        var array2 = [];
-        result = highNum - lowNum;
-        if (result !== 6174) {
-            result = String(result);
-            for (var i = 0; i < result.length; i++) {
-                array1.push(result[i]);
-            }
-            array1.sort(function(a, b){return b-a});
-            array2 = createMirrorArray(array1);
-            console.log(array1, array2);
-            number1 = createNumberFromArray(array1);
-            number2 = createNumberFromArray(array2);
-            createArraysFromSubtraction(number1, number2);
-        } else {
-            console.log(result);
-        }
-    }
-    
-    createArraysFromSubtraction(num1, num2);
+
+
 
 });
