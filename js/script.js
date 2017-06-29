dd64aef958576699007a5a84b6bd1643dbc935c0
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-06-29 16:13:56 +0200

    add function: DOM - retrieving numbers from input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 $(function(){
+ 
+    /*
+    DOM functions
+    */
 
-    var givenArray = [5, 2, 0, 0];
-    kaprekarGo(givenArray);
+    var resultBox = $('#result');
+    var submitBtn = $('#submit');
+    var inputs = $('input[name="digit"]');
 
+    var resultText = $('<p>');
+
+    submitBtn.on('click', function(e){
+        e.preventDefault();
+        var resultTextClone = resultText.clone(true);
+        var userArray = [];
+        inputs.each(function(){
+            userArray.push($(this).val());
+            $(this).val("");
+        });
+        userArray.sort(function(a, b){return b-a});
+        resultTextClone.text(createNumberFromArray(userArray));
+        resultTextClone.appendTo(resultBox);
+
+        kaprekarGo(userArray);
+    });
+    
 
     /*
     *** KAPREKAR ITERATION ***
@@ -12,13 +34,20 @@ $(function(){
       Step 4. Go back to step 2. 
     */
 
+    
+    var iterationNumber = 0;
+    
     function kaprekarGo(array) {
+        iterationNumber++;
         var result = 0;
         var array1 = array; // will be needed for step 2
         var array2 = createMirrorArray(array1); // will be needed for step 2
         var num1 = createNumberFromArray(array1);
         var num2 = createNumberFromArray(array2);
+        var resultTextClone = resultText.clone(true);
         result = num1 - num2; // step 3
+        resultTextClone.text("iteration no. " + iterationNumber + ": " + num1 + " - " + num2 + " = " + result);
+        resultTextClone.appendTo(resultBox);
         /* 
         condition: Kaprekar's constant is 6174 
         */
@@ -33,7 +62,9 @@ $(function(){
             // step 4
             kaprekarGo(newArray);
         } else {
-            console.log(result);
+            var finalResult = resultText.clone(true);
+            finalResult.text("Kaprekar's number " + result + " found after " + iterationNumber + " iterations!");
+            finalResult.appendTo(resultBox);
         }
     }
 
@@ -66,8 +97,5 @@ $(function(){
         result = Number(numString);
         return result;
     }
-
-
-
 
 });
