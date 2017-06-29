0e1c383a3013f4fac0cd61386bc5c177d35c7e55
Justyna Rudnicka-Staniewska <jete.justyce@gmail.com>
2017-06-29 12:58:55 +0200

    add function: Kaprekar iteration

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 $(function(){
 
-    var arr1 = [6, 4, 2, 1];
-    var arr2 = [];
+    var arr1 = [5, 2, 0, 0];
+    var arr2 = createMirrorArray(arr1);
 
-    function createMirrorArray(inputArray, outputArray){
+    var num1 = createNumberFromArray(arr1);
+    var num2 = createNumberFromArray(arr2);
+
+    // functions on arrays
+    function createMirrorArray(inputArray){
+        var outputArray = [];
         for (var i = 0; i < inputArray.length; i++) {
             outputArray.push(inputArray[i]);
         }
         outputArray.reverse();
+        return outputArray;
     }
-    createMirrorArray(arr1, arr2);
-
-    console.log(arr1);
-    console.log(arr2);
 
     function createNumberFromArray(inputArray){
         var numString = "";
@@ -21,9 +23,30 @@ $(function(){
             numString += inputArray[i];
         }
         result = Number(numString);
-        console.log(result);
+        return result;
     }
 
-    createNumberFromArray(arr1);
-    createNumberFromArray(arr2);
+    function createArraysFromSubtraction(highNum, lowNum) {
+        var result = 0;
+        var array1 = [];
+        var array2 = [];
+        result = highNum - lowNum;
+        if (result !== 6174) {
+            result = String(result);
+            for (var i = 0; i < result.length; i++) {
+                array1.push(result[i]);
+            }
+            array1.sort(function(a, b){return b-a});
+            array2 = createMirrorArray(array1);
+            console.log(array1, array2);
+            number1 = createNumberFromArray(array1);
+            number2 = createNumberFromArray(array2);
+            createArraysFromSubtraction(number1, number2);
+        } else {
+            console.log(result);
+        }
+    }
+    
+    createArraysFromSubtraction(num1, num2);
+
 });
